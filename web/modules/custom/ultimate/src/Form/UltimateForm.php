3d98e20b767e1dfe922db2f7c4f103c36d32de43
EvilArgest <argest7997@gmail.com>
2022-01-10 17:38:40 +0200

    wow ;)

--- web/modules/custom/ultimate/src/Form/UltimateForm.php
+++ web/modules/custom/ultimate/src/Form/UltimateForm.php
@@ -5,33 +5,28 @@ namespace Drupal\ultimate\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Provides table for.
+ */
 class UltimateForm extends FormBase {
 
   /**
    * Titles of the header.
-   *
-   * @var array
    */
   protected $titles;
 
   /**
    * Titles of the header.
-   *
-   * @var array
    */
   protected $intitles;
 
   /**
    * Amount of tables to be built.
-   *
-   * @var int
    */
   protected $tables = 1;
 
   /**
    * Amount of rows to be built for each table.
-   *
-   * @var int
    */
   protected $rows = 1;
 
@@ -90,14 +85,17 @@ class UltimateForm extends FormBase {
     $form['#suffix'] = '</div>';
     $form['#attached'] = ['library' => ['ultimate/ultimate_library']];
 
+    // Calling the functions for building the table.
     $this->inactiveCells();
     $this->buildTitles();
     $this->buildTable($form, $form_state);
 
+    // Adding buttons.
     $form['addTable'] = [
       '#type' => 'submit',
       '#value' => $this->t('Add Table'),
       '#submit' => ['::addTable'],
+      // We can add rows and tables even if we have errors.
       '#limit_validation_errors' => [],
       '#ajax' => [
         'callback' => '::submitAjaxForm',
@@ -112,6 +110,7 @@ class UltimateForm extends FormBase {
       '#type' => 'submit',
       '#value' => $this->t('Add Row'),
       '#submit' => ['::addRow'],
+      // We can add rows and tables even if we have errors.
       '#limit_validation_errors' => [],
       '#ajax' => [
         'callback' => '::submitAjaxForm',
@@ -162,6 +161,7 @@ class UltimateForm extends FormBase {
           '#step' => '0.01',
         ];
         if (array_key_exists($key, $this->intitles)) {
+          //Setting the default value to inactive cells.
           $value = $form_state->getValue($table_key . '][' . $i . '][' . $key, 0);
           $tablecell[$i][$key]['#default_value'] = round($value, 2);
           $tablecell[$i][$key]['#disabled'] = TRUE;
@@ -196,6 +196,7 @@ class UltimateForm extends FormBase {
     $values = [];
     $inactive_cells = $this->intitles;
     for ($i = $this->rows; $i > 0; $i--) {
+      // Setting array from active cells only.
       foreach ($array[$i] as $key => $value) {
         if (!array_key_exists($key, $inactive_cells)) {
           $values[] = $value;
@@ -216,24 +217,34 @@ class UltimateForm extends FormBase {
    * Validating the form.
    */
   public function validateForm(array &$form, FormStateInterface $form_state): void {
+    // Getting the values.
     $table_values = $form_state->getValues();
+    // The start point.
     $start_point = NULL;
+    // The end point.
     $end_point = NULL;
+    // Here we save the values from the table.
     $active_values = [];
     for ($i = 0; $i < $this->tables; $i++) {
+      // Getting the values from the table.
       $values = $this->arrayTransform($table_values[$i]);
+      // Saving the values here.
       $active_values[] = $values;
+      // Going through every cell in the table.
       foreach ($values as $key => $value) {
+        // Comparing every cell in the tables.
         for ($j = 0; $j < 12; $j++) {
           if (empty($active_values[0][$j]) !== empty($active_values[$i][$j])) {
-            $form_state->setErrorByName($i, 'Tables are not the same...');
+            $form_state->setErrorByName($i, 'Tables are not the same.');
           }
         }
+        // Getting the cell from where we start.
         if (!empty($value)) {
           $start_point = $key;
           break;
         }
       }
+      // If the start cell exist.
       if ($start_point !== NULL) {
         for ($l = $start_point; $l < count($values) + 1; $l++) {
           if (empty($values[$l])) {
@@ -242,10 +253,11 @@ class UltimateForm extends FormBase {
           }
         }
       }
+      // Going to the last filled +1 cell.
       if ($end_point !== NULL) {
         for ($f = $end_point; $f < count($values) + 1; $f++) {
           if (!empty($values[$f])) {
-            $form_state->setErrorByName($f, 'Form is not valid');
+            $form_state->setErrorByName($f, 'Form is not valid.');
           }
         }
       }
@@ -257,9 +269,12 @@ class UltimateForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $table_values = $form_state->getValues();
+      // Going through values of the table.
       foreach ($table_values as $table_key => $tablecell) {
         foreach ($tablecell as $row_key => $rows) {
+          // Getting the cell we need.
           $my_cell = $table_key . '][' . $row_key . '][';
+          // Setting up the q1-2-3-4 and ytd value.
           if (($rows['jan'] + $rows['feb'] + $rows['mar']) === 0) {
             $q1 = 0;
           }
@@ -290,7 +305,7 @@ class UltimateForm extends FormBase {
           else {
             $ytd = ($q1 + $q2 + $q3 + $q4 + 1) / 4;
           }
-
+          // Setting the value of the field.
           $form_state->setValue($my_cell . 'q1', $q1);
           $form_state->setValue($my_cell . 'q2', $q2);
           $form_state->setValue($my_cell . 'q3', $q3);
